f05ac5ce1870d0e5cd5c2d019cc49066d6cade72
Obbizzy_ <115794709+Obbiz@users.noreply.github.com>
2022-10-31 12:22:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = true //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['2348089700175'] //ur owner number
+global.ownername = "σввιzzу" //ur owner name
+global.ytname = "YT: σввιzzу" //ur yt chanel name
+global.socialm = "GitHub: Obbizzy_" //ur github or insta name
+global.location = "Nigeria, Kano, Yankaba" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
+global.owner = ['2348089700175'] //ur number
+global.ownernomer = "2348089700175" //ur number
+global.ownertag = '2348089700175' //ur tag number
+global.premium = ['2348089700175'] //ur premium number
+global.botname = 'σввιzzу Bot ' //ur bot name
+global.ownername = "σввιzzу"
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "σввιzzу Bot" //ur sticker watermark author
+global.wm = "σввιzzу Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
